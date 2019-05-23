72afe090133bc368d5eca7d0062c4beb4c20c8dd
Michael Ryan <michaeltheryan@gmail.com>
2019-05-23 19:48:13 -0400

    Reorganized architecture after rereading Learn
    Changed webpack output directory

--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -4,10 +4,10 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './client/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     publicPath: "/assets/"
   },
   devServer: {
