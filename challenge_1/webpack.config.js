bcf9e4e688eb5e2a817316663265dc7a0a4855c7
Michael Ryan <michaeltheryan@gmail.com>
2019-05-23 18:44:45 -0400

    Clean up of tracked files

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,35 @@
+﻿const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/assets/"
+  },
+  devServer: {
+    publicPath: 'http://localhost:8080/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin()
+  ]
+};
