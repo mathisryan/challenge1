445e5008618432aacca8a9d9d4b8a93e82db656f
Michael Ryan <michaeltheryan@gmail.com>
2019-05-23 20:08:19 -0400

    Inputted main React App component

--- challenge_1/client/App.jsx
+++ challenge_1/client/App.jsx
@@ -2,6 +2,23 @@ import { hot } from 'react-hot-loader/root';
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <div>???</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchTerm = ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({searchTerm: event.target.searchTerm})
+  }
+
+  handleSubmit(event) {
+    alert('Searching for ' + this.state.value);
+    event.preventDefault();
+  }
+}
 export default hot(App);
 
